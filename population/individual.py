f026ddcebae5f29b051288632a47b7ca1c623179
jluech <janik@luechinger.ch>
2020-07-21 18:00:53 +0200

    refactor: introduce default fitness for individual
    refs #106

--- population/individual.py
+++ population/individual.py
@@ -1,4 +1,7 @@
+DEFAULT_FITNESS = 0
+
+
 class Individual(object):
     def __init__(self, solution, fitness=None):
         self.solution = solution
-        self.fitness = fitness if fitness else 0
+        self.fitness = fitness if fitness else DEFAULT_FITNESS
