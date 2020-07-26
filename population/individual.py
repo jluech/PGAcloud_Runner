d55728531f033a004874e0c3e257c84bca8d2396
jluech <janik@luechinger.ch>
2020-07-26 21:37:28 +0200

    feat: add individuals JSON de-/serialization
    refs #106

--- population/individual.py
+++ population/individual.py
@@ -1,3 +1,5 @@
+from json import JSONEncoder, JSONDecoder
+
 DEFAULT_FITNESS = 0
 
 
@@ -5,3 +7,15 @@ class Individual(object):
     def __init__(self, solution, fitness=None):
         self.solution = solution
         self.fitness = fitness if fitness else DEFAULT_FITNESS
+
+
+# Make class JSON serializable:
+# https://pynative.com/make-python-class-json-serializable/
+class IndividualEncoder(JSONEncoder):
+    def default(self, obj):
+        return obj.__dict__
+
+
+class IndividualDecoder(JSONDecoder):
+    def default(self, obj):
+        return Individual(obj["solution"], obj["fitness"])
