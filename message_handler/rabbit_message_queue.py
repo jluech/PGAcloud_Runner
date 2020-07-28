124ee228cc1da5c5bfbc7df6c6ad304c3ad87933
jluech <janik@luechinger.ch>
2020-07-28 11:06:47 +0200

    feat: retrieve container conf, messaging source/target, adjust messaging
    refs #106

--- message_handler/rabbit_message_queue.py
+++ message_handler/rabbit_message_queue.py
@@ -11,12 +11,13 @@ QUEUE_NAME = "generation"  # TODO: retrieve each time from utils.get_messaging_s
 
 
 def receive_evaluated_individuals_callback(channel, method, properties, body):
+    queue_name = utils.get_messaging_source()
     payload_dict = json.loads(body.decode("utf-8"), cls=IndividualDecoder)
     population = payload_dict.get("payload")
     logging.info(body)  # TODO: remove
     logging.info(payload_dict)  # TODO: remove
     logging.info("rMQ:{queue_}: Received evaluated individuals: {pop_}".format(
-        queue_=QUEUE_NAME,
+        queue_=queue_name,
         pop_=population,
     ))
 
@@ -24,27 +25,23 @@ def receive_evaluated_individuals_callback(channel, method, properties, body):
     if received_all:
         channel.stop_consuming()
         logging.info("rMQ:{queue_}: Stopped consuming.".format(
-            queue_=QUEUE_NAME,
+            queue_=queue_name,
         ))
 
 
-def send_message_to_queue(channel, destinations, payload):
+def send_message_to_queue(channel, payload, next_recipient):
     # This will create the exchange if it doesn't already exist.
-    next_recipient = destinations.pop(0)
     channel.queue_declare(queue=next_recipient, auto_delete=True, durable=True)
 
     # Send message to given recipient.
-    logging.info("rMQ: Sending {body_} to destinations {dest_}.".format(
+    logging.info("rMQ: Sending {body_} to {dest_}.".format(
         body_=payload,
-        dest_=destinations,
+        dest_=next_recipient,
     ))
     channel.basic_publish(
         exchange="",
         routing_key=next_recipient,
-        body=json.dumps({
-            "destinations": destinations,
-            "payload": payload
-        }, cls=IndividualEncoder),
+        body=json.dumps(payload, cls=IndividualEncoder),
         # Delivery mode 2 makes the broker save the message to disk.
         # This will ensure that the message be restored on reboot even
         # if RabbitMQ crashes before having forwarded the message.
@@ -67,52 +64,49 @@ class RabbitMessageQueue(MessageHandler):
         channel = self.connection.channel()
 
         # Create queue for returning individuals as end of one generation.
-        channel.queue_declare(queue=QUEUE_NAME, auto_delete=True, durable=True)
+        queue_name = utils.get_messaging_source()
+        channel.queue_declare(queue=queue_name, auto_delete=True, durable=True)
 
         # Actively listen for messages in queue and perform callback on receive.
         channel.basic_consume(
-            queue=QUEUE_NAME,
+            queue=queue_name,
             on_message_callback=receive_evaluated_individuals_callback,
             auto_ack=True
         )
         logging.info("rMQ:{queue_}: Waiting for generation individuals feedback.".format(
-            queue_=QUEUE_NAME
+            queue_=queue_name
         ))
         channel.start_consuming()
 
         # Close connection when finished. TODO: check if prematurely closing connection
         self.connection.close()
 
-    def send_message(self, individual, remaining_destinations):
+    def send_message(self, individuals, next_recipient):
         # Define communication channel.
         channel = self.connection.channel()
         send_message_to_queue(
             channel=channel,
-            destinations=remaining_destinations,
-            payload=individual
+            payload=individuals,
+            next_recipient=next_recipient,
         )
 
-    def send_broadcast_to_init(self, amount, next_destinations):
+    def send_broadcast_to_init(self, amount):
         # Define communication channel.
         channel = self.connection.channel()
 
         # Create the exchange if it doesn't exist already.
-        exchange_name = next_destinations.pop(0)
+        exchange_name = utils.get_messaging_init_gen()
         channel.exchange_declare(exchange=exchange_name, exchange_type="fanout", auto_delete=True, durable=True)
 
         # Send message to given recipient.
-        logging.info("rMQ: Sending '{body_}' to '{init_}' with destinations {dest_}.".format(
+        logging.info("rMQ: Sending '{body_}' to '{init_}'.".format(
             body_=amount,
             init_=exchange_name,
-            dest_=next_destinations,
         ))
         channel.basic_publish(
             exchange=exchange_name,
             routing_key="",
-            body=json.dumps({
-                "destinations": next_destinations,
-                "payload": amount
-            }),
+            body=json.dumps(amount),
             # Delivery mode 2 makes the broker save the message to disk.
             # This will ensure that the message be restored on reboot even
             # if RabbitMQ crashes before having forwarded the message.
